928b00247128f8330330455b10793a2945fd0a7b
Lucas <lucas.bardelli@hotmail.com>
2024-09-03 23:07:16 -0300

    Add project files.

---
+++ FI.WebAtividadeEntrevista/Validators/CpfValidation.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+public class CpfValidation : ValidationAttribute
+    {
+    public string Cpf { get; set; }
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+        Cpf = value.ToString();
+        if (string.IsNullOrEmpty(Cpf))
+            {
+            return ValidationResult.Success;
+            }
+
+        Cpf = Cpf.Replace(".", "").Replace("-", "");
+
+        if (Cpf.Length != 11 || !Regex.IsMatch(Cpf, @"^\d{11}$"))
+            {
+            return new ValidationResult("CPF inválido.");
+            }
+
+        int[] multiplicador1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        int[] multiplicador2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+        string tempCpf = Cpf.Substring(0, 9);
+        int soma = 0;
+
+        for (int i = 0; i < 9; i++)
+            soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+        int resto = soma % 11;
+        if (resto < 2)
+            resto = 0;
+        else
+            resto = 11 - resto;
+
+        string digito = resto.ToString();
+
+        tempCpf = tempCpf + digito;
+        soma = 0;
+
+        for (int i = 0; i < 10; i++)
+            soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+        resto = soma % 11;
+        if (resto < 2)
+            resto = 0;
+        else
+            resto = 11 - resto;
+
+        digito = digito + resto.ToString();
+
+        if (!Cpf.EndsWith(digito))
+            {
+            return new ValidationResult("CPF inválido.");
+            }
+
+        return ValidationResult.Success;
+        }
+    }
