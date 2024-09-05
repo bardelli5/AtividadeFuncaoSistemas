bb20d478428bc1f6d2e9a0a0355a956cec39a32c
Lucas <lucas.bardelli@hotmail.com>
2024-09-04 22:44:55 -0300

    Adicionando modal de beneficiarios, com sua respectiva controller e arquivo js

---
+++ FI.WebAtividadeEntrevista/Models/BeneficiarioModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAtividadeEntrevista.Models
+{
+    /// <summary>
+    /// Classe de Modelo de Cliente
+    /// </summary>
+    public class BeneficiarioModel
+        {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        [Required(ErrorMessage = "O Nome é obrigatório")]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Cpf
+        /// </summary>
+        [Required(ErrorMessage = "O CPF é obrigatório")]
+        [CpfValidation(ErrorMessage = "CPF inválido.")]
+        public string Cpf { get; set; }
+
+        }    
+}
---
+++ FI.WebAtividadeEntrevista/Scripts/Beneficiarios/FI.Beneficiarios.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    //function modalBeneficiarios(idCliente) {
+    //    $('#gridClientes').jtable({
+    //        actions: {
+    //            listAction: urlClienteList,
+    //        },
+    //        fields: {
+    //            Nome: {
+    //                title: 'Cpf',
+    //                width: '50%'
+    //            },
+    //            Email: {
+    //                title: 'Nome',
+    //                width: '35%'
+    //            },
+    //            Alterar: {
+    //                title: '',
+    //                display: function (data) {
+    //                    return '<button onclick="window.location.href=\'' + urlAlteracao + '/' + data.record.Id + '\'" class="btn btn-primary btn-sm">Alterar</button>';
+    //                    return '<button onclick="window.location.href=\'' + urlAlteracao + '/' + data.record.Id + '\'" class="btn btn-primary btn-sm">Excluir</button>';
+    //                }
+    //            }
+    //        }
+    //    });
+    //}
+
+    $("#CpfBeneficiario").on("input", function () {
+        var cpf = $(this).val();
+        cpf = cpf.replace(/\D/g, "")
+            .replace(/(\d{3})(\d)/, "$1.$2")
+            .replace(/(\d{3})(\d)/, "$1.$2")
+            .replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+        $(this).val(cpf);
+    });
+
+    //Load student list from server
+    //if (document.getElementById("gridClientes"))
+    //    $('#gridClientes').jtable('load');
+})
